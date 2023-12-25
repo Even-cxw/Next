40f9113021bcf401c3d4437c7cf926e57d3c5a3c
陈学伟 <chenxuewei1@eccom.com.cn>
2023-12-25 17:01:23 +0800

    创建项目5

--- nextjs-demo4/next.config.js
+++ nextjs-demo4/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
 
+const nextConfig = {
+  sassOptions: {
+    sassOptions: {
+      includePaths: [
+        path.join(__dirname, './src/styles/_vars.scss'),
+        path.join(__dirname, './src/styles/_mixins.scss'),
+      ],
+    },
+  },
+}
 module.exports = nextConfig
